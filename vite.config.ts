ad420fd8c85aa963522ebc0502fb3a3fee3d4f9c
faaiq <66476388+verhaxity@users.noreply.github.com>
2025-03-22 13:05:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // Updated base path for GitHub Pages
+  base: '/image-template-tweak/', // Updated base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
